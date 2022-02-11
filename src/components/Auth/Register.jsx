0b4c8f3774ca1050f858740efb7c994d9c608825
ankitt01 <ankitrawat0102@gmail.com>
2022-02-11 15:55:13 +0530

    integrated tailwind css

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,30 +1,32 @@
-import {Grid, Form, Segment, Button, Header, Message, Icon, GridColumn} from "semantic-ui-react"
+// import {Grid, Form, Segment, Button, Header, Message, Icon, GridColumn} from "semantic-ui-react"
 import { Link } from "react-router-dom"
 const Register = () => {
-    const []
-    const handleChange = () => {
-
-    }
+    // const []
+    // const handleChange = () => {
+    //     console.log("Hello")
+    // }
     return (
-        <Grid textAlign="center" verticalAlign="middle" className="app">
-            <GridColumn style={{maxWidth: 450}}>
-                <Header as="h2" icon color="orange" textAlign="center">
-                    <Icon name="puzzle piece" color="orange" />
-                    Register for DevChat
-                </Header>
-                <Form size="large">
-                    <Segment stacked>
-                        <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" onChange={handleChange} type="text"/>
-                        <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="Email Address" onChange={handleChange} type="email"/>
-                        <Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="Password" onChange={handleChange} type="password"/>
-                        <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" placeholder="Password Confirmation" onChange={handleChange} type="password"/>
-                        <Button color="orange" fluid size="large" >Submit</Button>
-                    </Segment>
-                </Form>
-                <Message>Already a user?<Link to="/login">Login</Link></Message>
-            </GridColumn>
+        // <Grid textAlign="center" verticalAlign="middle" className="app">
+        //     {/* <GridColumn style={{maxWidth: 450}}>
+        //         <Header as="h2" icon color="orange" textAlign="center">
+        //             <Icon name="puzzle piece" color="orange" />
+        //             Register for DevChat
+        //         </Header>
+        //         <Form size="large">
+        //             <Segment stacked>
+        //                 <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" onChange={handleChange} type="text"/>
+        //                 <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="Email Address" onChange={handleChange} type="email"/>
+        //                 <Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="Password" onChange={handleChange} type="password"/>
+        //                 <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" placeholder="Password Confirmation" onChange={handleChange} type="password"/>
+        //                 <Button color="orange" fluid size="large" >Submit</Button>
+        //             </Segment>
+        //         </Form>
+        //         <Message>Already a user?<Link to="/login">Login</Link></Message>
+        //     </GridColumn> */}
+        //     Register
+        // </Grid>
 
-        </Grid>
+        <h1>Hello</h1>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import './index.css';
-import 'semantic-ui-css/semantic.min.css'
+// import 'semantic-ui-css/semantic.min.css'
 import App from './components/App';
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
