ec98a9fcfae31076278aaa8ac828b3c63a0c05e5
ankitt01 <ankitrawat0102@gmail.com>
2022-02-11 21:36:32 +0530

    Tailwind styling register component

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,33 +1,47 @@
-// import {Grid, Form, Segment, Button, Header, Message, Icon, GridColumn} from "semantic-ui-react"
 import { Link } from "react-router-dom"
-const Register = () => {
-    // const []
-    // const handleChange = () => {
-    //     console.log("Hello")
-    // }
-    return (
-        // <Grid textAlign="center" verticalAlign="middle" className="app">
-        //     {/* <GridColumn style={{maxWidth: 450}}>
-        //         <Header as="h2" icon color="orange" textAlign="center">
-        //             <Icon name="puzzle piece" color="orange" />
-        //             Register for DevChat
-        //         </Header>
-        //         <Form size="large">
-        //             <Segment stacked>
-        //                 <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" onChange={handleChange} type="text"/>
-        //                 <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="Email Address" onChange={handleChange} type="email"/>
-        //                 <Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="Password" onChange={handleChange} type="password"/>
-        //                 <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left" placeholder="Password Confirmation" onChange={handleChange} type="password"/>
-        //                 <Button color="orange" fluid size="large" >Submit</Button>
-        //             </Segment>
-        //         </Form>
-        //         <Message>Already a user?<Link to="/login">Login</Link></Message>
-        //     </GridColumn> */}
-        //     Register
-        // </Grid>
 
-        <h1>Hello</h1>
-    )
+const Register = () => {
+  return (
+
+    <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div class="max-w-md w-full space-y-8">
+        <div>
+          <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
+          <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Register your account</h2>
+        </div>
+        <form class="mt-8 space-y-6 border-4 p-4" action="#" method="POST">
+          <div class="rounded-md shadow-sm -space-y-px">
+          <div>
+              <label for="email-address" class="sr-only">Username</label>
+              <input id="email-address" name="username" type="text" autocomplete="email" required class="appearance-none rounded-none relative block mb-2 w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Username" />
+            </div>
+            <div>
+              <label for="email-address" class="sr-only">Email address</label>
+              <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full mb-2 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" />
+            </div>
+            <div>
+              <label for="password" class="sr-only">Password</label>
+              <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full mb-2 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
+            </div>
+            <div>
+              <label for="password" class="sr-only">Confirm Password</label>
+              <input id="password" name="passwordConfirmation" type="password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Confirm Password" />
+            </div>
+          </div>
+
+          <div>
+            <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Register
+            </button>
+          </div>
+        </form>
+        <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-black hover:text-white bg-indigo-200 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Already a User?
+              <Link to="/login">Login</Link>
+            </button>
+      </div>
+    </div>
+  )
 }
 
 export default Register
